b22d229e23b88ce29a909b843777f51e0716ffa3
dagothar <dagothar@gmail.com>
2016-03-19 14:31:51 +0100

    added tests

---
+++ src/main/java/gb/TestController.java
@@ -0,0 +1,16 @@
+package gb;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+  
+  public static String message = "Hello!";
+  
+  @RequestMapping("/test")
+  public String test() {
+    return message;
+  }
+  
+};
