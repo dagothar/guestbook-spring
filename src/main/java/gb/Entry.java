2a613ccca8e45a3bcbccba4c6b0660ce9ef712d4
dagothar <dagothar@gmail.com>
2016-03-20 10:53:05 +0100

    added exception handler

--- src/main/java/gb/Entry.java
+++ src/main/java/gb/Entry.java
@@ -1,36 +1,41 @@
 package gb;
 
+import javax.validation.constraints.*;
+
 public class Entry {
+
   private int id;
+
+  @NotNull(message = "Message cannot be empty!")
   private String message;
-  
+
   public Entry() {
   }
-  
+
   public Entry(int id, String message) {
     this.id = id;
     this.message = message;
   }
-  
+
   public int getId() {
     return id;
   }
-  
+
   public void setId(int id) {
     this.id = id;
   }
-  
+
   public String getMessage() {
     return message;
   }
-  
+
   public void setMessage(String message) {
     this.message = message;
   }
-  
+
   @Override
   public String toString() {
     return String.format("Entry[id='%d', message='%s']", id, message);
   }
-  
+
 };
--- src/main/java/gb/EntryController.java
+++ src/main/java/gb/EntryController.java
@@ -1,29 +1,39 @@
 package gb;
 
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import javax.validation.Valid;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @RestController
 public class EntryController {
-  
+
   @Autowired
   EntryRepository entryRepository;
-  
+
   @RequestMapping(value = "/entries", method = RequestMethod.GET)
   public List<Entry> getAllEntries() {
     return entryRepository.findAll();
   }
-  
+
   @RequestMapping(value = "/entries", method = RequestMethod.PUT)
-  public void addNewEntry(@RequestBody Entry entry) {
+  public void addNewEntry(@RequestBody @Valid Entry entry) {
     entryRepository.addEntry(entry);
   }
-  
+
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleException(MethodArgumentNotValidException exception) {
+    return "error";
+  }
+
 };
